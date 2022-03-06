dd3898834429b8051bfee1e47f2f79cabf121519
Francesco Maresca <97321140+programmerhelloworld@users.noreply.github.com>
2022-03-06 15:53:48 +0100

    Create main.cpp

---
+++ C++ Version/main.cpp
@@ -0,0 +1,115 @@
+//
+//  main.cpp
+//  AreaCalculator
+//
+//  Created by @programmerhelloworld (Francesco Maresca) on 03/03/22.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    
+    string choose;
+    cout << "Rectangle area (rectangle), Triangle area (triangle), Square(square): " << endl;
+    cin >> choose;
+    
+    if (choose == "rectangle"){
+        
+        float base;
+        cout << "Insert the base: "  << endl;
+        cin >> base;
+        
+        float height;
+        cout << "Insert the height: "  << endl;
+        cin >> height;
+        
+        float area;
+        area = base * height;
+        
+        cout << "Rectangle area is " << area << endl;
+        
+    
+        
+    } else if (choose == "triangle"){
+       
+        cout << "Choose the type of triangle: Square Triangle (squaretriangle), Scalene Triangle (scalenetriangle), Isosceles Triangle (isoscelestriangle): " << endl;
+        string choosetr;
+        cin >> choosetr;
+        
+       
+        
+        if (choosetr == "squaretriangle"){
+            
+            float cathetus1;
+            cout << "Insert the first cathetus: " << endl;
+            cin >> cathetus1;
+            
+            
+            float cathetus2;
+            cout << "Insert the second cathetus: " << endl;
+            cin >> cathetus2;
+            float area;
+            
+            area = (cathetus1 * cathetus2)/2;
+            cout << "The square triangle's area is " << area << endl;
+            
+            
+        } else if (choosetr == "scalenetriangle"){
+            
+            float cathetus1;
+            cout << "Insert the first cathetus: " << endl;
+            cin >> cathetus1;
+            
+            
+            float cathetus2;
+            cout << "Insert the second cathetus: " << endl;
+            cin >> cathetus2;
+            float area;
+            
+            area = (cathetus1 * cathetus2)/2;
+            cout << "The scalene triangle's area is " << area << endl;
+            
+            
+            
+        } else if (choosetr == "isoscelestriangle"){
+            
+            float base;
+            cout << "Insert the base: "  << endl;
+            cin >> base;
+            
+            float height;
+            cout << "Insert the height: "  << endl;
+            cin >> height;
+            
+            float area;
+            area = base * height;
+            
+            cout << "The isosceles triangle's area is " << area << endl;
+            
+            
+        }
+        
+        
+    } else if (choose == "square"){
+        
+        float side;
+        float area;
+        
+        cout << "Insert the square side: " << endl;
+        cin >> side;
+        
+        area = side * side;
+        
+        cout << "Area is " << area << endl;
+        
+        
+        
+    }
+    
+    
+    
+    
+    
+}
